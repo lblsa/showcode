67ba2a98401ef6b876412bc2c86dd3b5f4b5f413
Lblsa <roman.efimushkin@gmail.com>
2012-09-19 13:35:17 +0000

    ЧПУ

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'showcode.ru',
+	'name'=>'showcode.azurewebsites.net',
 	'sourceLanguage' => 'ru',
     'language' => 'ru',
 
