afa0b00294086132317b5665629eb7018cd786e0
Dasha <dasha@dasha.(none)>
2012-11-04 16:33:56 +0400

    [P2] (Bug) После отправки Письма с билетами из события, настроен правильный редирект после отправки письма с билетами

--- protected/controllers/EventsController.php
+++ protected/controllers/EventsController.php
@@ -1201,13 +1201,37 @@ class EventsController extends Controller
                 {
                     $buy_place[] = ($tr[$i]->column-1)*$model->place + $tr[$i]->place;
                 }
+				
+				//получили данные о доп оргах
+				$modelOrgAll = EventOrg::model()->findAllByAttributes(array('id_event'=>$id));
+				
+				$ids = array();
+				$values = array();
+				
+				if(!empty($modelOrgAll))
+				{
+					foreach ($modelOrgAll as $key=>$orgs)
+					{
+						$user = User::model()->findByAttributes(array('user_id'=>$orgs->id_org));
+					
+						$ids[] = $orgs->id_org;
+						$values[$key]['name'] = $user->name;
+						$values[$key]['phone'] = $user->phone;
+					}
+				}
+				$log = new TransactionLog;
+				$ticket = $this->loadTicket($id);
+				$this->redirect(array('view', 'id'=>$id));
 
-                $this->render(Yii::app()->mf->siteType(). '/view',array(
-                            'model'=>$event,
-                            'ticket'=>$this->loadTicket($id),
-                            'log'=>new TransactionLog,
+               /* $this->render(array(Yii::app()->mf->siteType(). '/view',
+							'model'=>$event,
+                            'ticket'=>$ticket,
+                            'log'=>$log,
                             'buy_place'=>$buy_place,
-                            ));
+							'ids'=>$ids,
+							'values'=>$values,
+                        ));
+						*/
             }else
                 $errors = 1;
         }
